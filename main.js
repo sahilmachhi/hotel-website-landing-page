b9aecd50d792ad0b147c380531889794b77cf3d7
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-01-04 00:23:29 +0530

    commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function toggleNav() {
-    const navbarLinks = document.querySelector('.navbar-links');
-    navbarLinks.classList.toggle('active');
-}
+let navBtn = document.getElementById('menu-btn');
+let navbarLinks = document.querySelector('.navbar-links');
+
+navBtn.addEventListener('click', function () {
+  navbarLinks.classList.toggle('navbar-links-active');
+});
