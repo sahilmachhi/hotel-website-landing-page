0cb65d635e24071fabb6ee0eb8d5c92574efd0e4
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-02-10 18:50:20 +0530

    commit

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 let navBtn = document.getElementById('menu-btn');
 let navbarLinks = document.querySelector('.navbar-links');
+let navClicked = document.querySelectorAll('a');
+
 
 navBtn.addEventListener('click', function () {
   navbarLinks.classList.toggle('navbar-links-active');
+});
+
+navClicked.forEach(navClick => {
+  navClick.addEventListener('click', function () {
+    console.log("clicked");
+    navbarLinks.classList.toggle('navbar-links-active');
+  });
 });
